b9985d2fffc351f593c31322c0902194484e7424
Vimal Menon <vimal.webs@gmail.com>
2026-04-03 17:39:25 +0800

    feat: add Env constants for API_URL and display on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,14 @@
-
+import { Env } from "../../constants";
 
 export default function admin() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white">
       <div className="text-center">
         This is Admin Page
+        <div>
+          {Env.API_URL}
+        </div>
+        
       </div>
     </div>
   );
