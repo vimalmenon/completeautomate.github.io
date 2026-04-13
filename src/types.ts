22e694f06d9eec34ef16516f839f05c3a125d906
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-13 18:35:14 +0800

    updated types

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,20 @@
-export type JobStatusType = "Running" | "Queued" | "Completed" | "Failed"
+export type JobStatusType = "Running" | "Queued" | "Completed" | "Failed";
 
 export interface IJob {
-    id: string,
-    status: JobStatusType,
-    type: string,
-    taskData: Record<string, any>,
-    description: string,
-    createdAt: string,
-    failedCount: number,
-    pendingOn: string[],
-    completedAt?: string,
-    errorMsg?: string
+  id: string;
+  status: JobStatusType;
+  type: string;
+  taskData: Record<string, any>;
+  description: string;
+  createdAt: string;
+  failedCount: number;
+  pendingOn: string[];
+  completedAt?: string;
+  errorMsg?: string;
 }
 
-export interface IYouTubeVideo {
-}
+export interface IYouTubeVideo {}
 
-export interface IYouTubeChannel {
-}
+export interface IYouTubeChannel {}
+
+export interface IPrompt {}
