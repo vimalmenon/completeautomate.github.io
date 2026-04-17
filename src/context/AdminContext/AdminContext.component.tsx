374b711c97b7189777fdd5082a086835dd7801f6
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-17 12:24:14 +0800

    feat: implement AdminContext and provider for managing loading state

--- src/context/AdminContext/AdminContext.component.tsx
+++ src/context/AdminContext/AdminContext.component.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+import React from 'react';
+
 import { IReactChildren } from '@types';
 
-export const AdminContext: React.FC<IReactChildren> = ({children}) => {
-    return <>{children}</>;
+import { AdminContextProvider } from './AdminContext.service';
+
+export const AdminContext: React.FC<IReactChildren> = ({ children }) => {
+  const [loading] = React.useState<boolean>(false);
+  return <AdminContextProvider.Provider value={loading}>{children}</AdminContextProvider.Provider>;
 };
